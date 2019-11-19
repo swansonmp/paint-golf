0240fe3b47ab3d450b009ea5f5eb82de7ace5c25
Matthew Swanson <54331919+swansonmp@users.noreply.github.com>
2019-11-19 08:43:48 -0500

    Draw static power bar

--- src/powerbar.js
+++ src/powerbar.js
@@ -1,6 +1,12 @@
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+const BAR_X = GAME_WIDTH / 4;
+const BAR_Y = GAME_HEIGHT - 50;
+const BAR_WIDTH = 400;
+const BAR_HEIGHT = 30;
+const PADDING = 2;
+
 export default class PowerBar {
   constructor() {
     
@@ -13,7 +19,19 @@ export default class PowerBar {
   }
   
   draw(ctx) {
+    //draw outline
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fillRect(BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
+
+    //draw center
+    ctx.fillStyle = "rgba(128,82,0,1)";
+    ctx.fillRect(BAR_X + PADDING, BAR_Y + PADDING, BAR_WIDTH - PADDING * 2, BAR_HEIGHT - PADDING * 2);
     
+    //draw active
+    //ctx.fillStyle = "rgba(255,165,0,1)";
+    
+    //draw cursor
+    //TODO
   }
   
   update(deltaTime) {
