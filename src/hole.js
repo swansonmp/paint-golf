23488f7e8624f05513650ee07566b2911b09c6c8
Matthew Swanson <54331919+swansonmp@users.noreply.github.com>
2019-11-20 10:02:12 -0500

    Start on hole loading. Thinking about refactor

--- src/hole.js
+++ src/hole.js
@@ -1,13 +1,27 @@
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+const PIXELTYPE = {
+  TEE: 0,
+  HOLE: 1,
+  GREEN: 2,
+  FAIRWAY: 3,
+  ROUGH: 4,
+  BUNKER: 5,
+  WATER: 6
+};
+
 export default class Hole {
   constructor(name) {
     this.image = document.getElementById(name);
     
+    //this.map = loadHole(image);
+  }
+  
+  loadHole(image) {
     
   }
-
+  
   draw(ctx) {
     ctx.drawImage(this.image, 0, 0, GAME_WIDTH, GAME_HEIGHT);
   }
